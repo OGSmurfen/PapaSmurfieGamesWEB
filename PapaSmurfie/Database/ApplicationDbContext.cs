54c7b24a35129ba51de989ab68d47a4ad219a1ba
Ilia Chakarov <114438460+OGSmurfen@users.noreply.github.com>
2024-08-26 14:25:15 +0300

    Add project files.

---
+++ PapaSmurfie/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using PapaSmurfie.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace PapaSmurfie.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<GameModel> Games { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+             
+        }
+    }
+}
---
+++ PapaSmurfie/Models/GameModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PapaSmurfie.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; } = null!;
+        public string GameURL { get; set; } = null!;
+        public string GameDescription { get; set; } = null!;
+        public string GameCompanyName { get; set; } = "PapaSmurfie";
+        public double GameRating { get; set; }
+        public double RoundedGameRating { get { return Math.Round((GameRating * 10) / 10); } }
+    }
+}
